005b48dbdfd4bc360fc0cc7a411ec2719651047f
joaogui1 <joaoguilhermearujo@gmail.com>
2019-08-29 15:13:26 -0300

    new version

---
+++ build/lib/fastax/layers/__init__.py
@@ -0,0 +1,19 @@
+from fastax.layers.attention import *
+from fastax.layers.base import *
+from fastax.layers.core import *
+from fastax.layers.combinators import *
+from fastax.layers.convolution import *
+from fastax.layers.core import *
+from fastax.layers.initializers import *
+from fastax.layers.normalization import *
+from fastax.layers.pooling import *
+from fastax.layers.reversible import *
+from fastax.layers.rnn import *
+
+
+
+
+
+
+
+
